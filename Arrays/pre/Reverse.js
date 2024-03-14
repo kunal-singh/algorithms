e3db82d1a02c5db7c967cb779fb40c043f00bb3a
kunal-singh <hi@thekunalsingh.com>
2024-03-14 18:13:07 +0530

    day-2:- installed vitest

--- Arrays/pre/Reverse.js
+++ Arrays/pre/Reverse.js
@@ -1,5 +1,21 @@
-const Reverse = () => {
-  console.log('kunal');
+/**
+ * Function to reverse a given Array
+ * @param {Array} array - Given array to reverse
+ * @returns {Array}
+ */
+const Reverse = (array) => {
+  if(array === null || array === undefined){
+    throw new Error(`unefined or null array provided`);
+  }
+  if(!Array.isArray(array)){
+    throw new error(`provided argument not an Array`);
+  }
+  let newArr = [];
+  for (let i = array.length - 1; i >= 0 ; i-- ){
+     newArr.push(array[i]);
+  }
+  return newArr;
 }
 
 export default Reverse;
+
---
+++ Arrays/tests/reverse.test.js
@@ -0,0 +1,19 @@
+import Reverse from "../pre/Reverse.js";
+import { expect, test } from 'vitest'
+
+test(`Running test for Reverse - expected`,() => {
+    expect(Reverse([1,2,3])).toEqual([3,2,1]);
+})
+
+test(`Running test for reverse - undefined input`,() => {
+        expect(() => Reverse()).toThrowError();
+})
+
+test(`Running test for reverse - null input`,() => {
+    expect(() => Reverse()).toThrowError();
+})
+
+test(`Running test for reverse - incompatible param`,() => {
+    expect(() => Reverse()).toThrowError();
+})
+
