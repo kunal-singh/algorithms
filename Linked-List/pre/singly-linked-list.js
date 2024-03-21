f2790d30b84aed58cb9a8f4768ba22f60ed40785
kunal-singh <hi@thekunalsingh.com>
2024-03-21 14:39:31 +0530

    day-5:- linked list basic operations wip

--- Linked-List/pre/singly-linked-list.js
+++ Linked-List/pre/singly-linked-list.js
@@ -66,9 +66,46 @@ function find(data) {}
 
 function findNth(index) {}
 
-function deleteValue(data) {}
+function removeFirst() {
+  if (this.size === 0) {
+    throw new Error('Empty Linked List');
+  }
+  const currentHead = this.headNode;
+  delete this.headNode;
+  this.headNode = currentHead.next;
+  this.size -= 1;
+  if (this.size === 0) {
+    this.tailNode = null;
+  }
 
-function deleteAt(index) {}
+  return currentHead.data;
+}
+
+function removeLast() {
+  if (this.size === 0) {
+    throw new Error('Empty Linked List');
+  }
+  let tempNode = this.headNode;
+  while (true) {
+    if (!tempNode.next || tempNode.next.next === null) {
+      break;
+    }
+    tempNode = tempNode.next;
+  }
+  if (!tempNode.next) {
+    return this.removeFirst();
+  }
+  const { data } = tempNode.next;
+  delete tempNode.next;
+  tempNode.next = null;
+
+  this.size -= 1;
+  return data;
+}
+
+function remove(data) {}
+
+function removeAt(index) {}
 
 function asArray() {
   if (this.size === 0) {
@@ -85,7 +122,18 @@ function asArray() {
 
 function reverse() {}
 
-function destroy() {}
+function destroy() {
+  if (this.headNode) {
+    while (true) {
+      const tempHead = this.headNode.next;
+      delete this.headNode;
+      if (!tempHead) break;
+    }
+  }
+  this.tailNode = null;
+  this.headNode = null;
+  this.size = 0;
+}
 
 const SinglyLinkedList = () => {
   let headNode = null;
@@ -106,8 +154,10 @@ const SinglyLinkedList = () => {
     isEmpty,
     find,
     findNth,
-    deleteValue,
-    deleteAt,
+    remove,
+    removeAt,
+    removeFirst,
+    removeLast,
     asArray,
     reverse,
     destroy
