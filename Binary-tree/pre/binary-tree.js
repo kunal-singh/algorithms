a8ee61d650402f47ef0018ab51db226c47090458
kunal-singh <hi@thekunalsingh.com>
2024-04-01 10:33:27 +0530

    added binary tree test cases

---
+++ Binary-tree/pre/binary-tree.js
@@ -0,0 +1,38 @@
+function add() {}
+
+function remove() {}
+
+function search() {}
+
+function level() {}
+
+function inorderIterator() {}
+
+function preorderIterator() {}
+
+function postorderIterator() {}
+
+function levelorderIterator() {}
+
+function isFull() {}
+
+function isComplete() {}
+
+function toArray(iterator) {}
+
+const BinarySearchTree = () => {
+  return {
+    add,
+    remove,
+    search,
+    level,
+    inorderIterator,
+    preorderIterator,
+    postorderIterator,
+    levelorderIterator,
+    isFull,
+    isComplete
+  };
+};
+
+export default BinarySearchTree;
--- Binary-tree/test/binary-tree.test.js
+++ Binary-tree/test/binary-tree.test.js
@@ -1,8 +1,33 @@
 import { describe, it, expect } from 'vitest';
+import BinarySearchTree from '../pre/binary-tree.js';
 
 describe('Binary Search Tree', () => {
   it('Check add', () => {
-    const list = LinkedList([1, 2, 3, 4, 5]);
-    expect(list.asArray()).toEqual([1, 2, 3, 4, 5]);
+    const tree = BinarySearchTree();
+    expect(tree.level()).toEqual(0);
+    tree.add(1);
+    tree.add(2);
+    tree.add(3);
+    tree.add(4);
+    tree.add(5);
+    expect(tree.level()).toEqual(4);
+  });
+  it('Check remove', () => {
+    const tree = BinarySearchTree();
+    expect(tree.level()).toEqual(0);
+    tree.add(1);
+    tree.add(2);
+    tree.add(3);
+    tree.add(4);
+    tree.add(5);
+
+    tree.remove(1);
+    expect(tree.level()).toEqual(3);
+
+    tree.remove(2);
+    expect(tree.level()).toEqual(3);
+
+    tree.remove(70);
+    expect(tree.level()).toEqual(3);
   });
 });
