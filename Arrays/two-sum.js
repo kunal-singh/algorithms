b70b9432a0be847a11633d03b023f4ac3e3fb2a0
kunal-singh <hi@thekunalsingh.com>
2024-04-08 20:44:44 +0530

    two sum version 1 success

--- Arrays/two-sum.js
+++ Arrays/two-sum.js
@@ -1,3 +1,13 @@
-function TwoSum() {}
+function TwoSum(nums, target) {
+  const sumValues = [];
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = i + 1; j < nums.length; j += 1) {
+      if (nums[i] + nums[j] === target) {
+        sumValues.push(...[i, j]);
+      }
+    }
+  }
+  return sumValues.length ? sumValues : null;
+}
 
 export default TwoSum;
--- Arrays/two-sum.test.js
+++ Arrays/two-sum.test.js
@@ -4,6 +4,20 @@ import TwoSum from './two-sum.js';
 describe('Two sum', () => {
   it('test case 1', () => {
     const arr1 = [1, 6, 7, 8, 9, 10];
-    expect(TwoSum(arr1, 9)).toEqual([1, 8]);
+    expect(TwoSum(arr1, 9)).toEqual([0, 3]);
+    expect(TwoSum(arr1, 10)).toEqual([0, 4]);
+    expect(TwoSum(arr1, 21)).toBeNull();
+  });
+  it('test case 2', () => {
+    const arr1 = [2, 7, 11, 15];
+    expect(TwoSum(arr1, 9)).toEqual([0, 1]);
+  });
+  it('test case 3', () => {
+    const arr1 = [3, 2, 4];
+    expect(TwoSum(arr1, 6)).toEqual([1, 2]);
+  });
+  it('test case 4', () => {
+    const arr1 = [3, 3];
+    expect(TwoSum(arr1, 6)).toEqual([0, 1]);
   });
 });
