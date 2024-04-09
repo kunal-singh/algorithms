bd9669a207061e38b0c3c448a321847579bc765e
kunal-singh <hi@thekunalsingh.com>
2024-04-09 14:04:09 +0530

    convert stack from factory to prototype

--- Stack/pre/stack.js
+++ Stack/pre/stack.js
@@ -1,3 +1,16 @@
-function Stack() {}
+// Functions: push, pop, peek, view, length
+
+const Stack = (function () {
+  function Stack() {}
+  Stack.prototype.push = function () {};
+
+  Stack.prototype.pop = function () {};
+
+  Stack.prototype.peek = function () {};
+
+  Stack.prototype.view = function () {};
+
+  Stack.prototype.length = function () {};
+})();
 
 export default Stack;
