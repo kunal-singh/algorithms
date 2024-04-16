30dc5eec7e1a7a7b671e33877e68a77bf82c3959
kunal-singh <hi@thekunalsingh.com>
2024-04-16 16:25:49 +0530

    graph DS wip

--- Graph/pre/graph.js
+++ Graph/pre/graph.js
@@ -1,11 +1,39 @@
 const Graph = (function () {
-  function Graph() {}
+  function Graph() {
+    this.adjacencyMap = new Map();
+  }
 
-  Graph.prototype.addVertex = function (vertex) {};
+  Graph.prototype.addVertex = function (vertex) {
+    if (vertex && !this.adjacencyMap.get(vertex)) {
+      this.adjacencyMap.set(vertex, []);
+    }
+  };
 
-  Graph.prototype.addEdge = function (v1, v2) {};
+  Graph.prototype.addEdge = function (v1, v2) {
+    if (v1 && v2 && this.adjacencyMap.has(v1) && this.adjacencyMap.has(v2)) {
+      const v1Edges = this.adjacencyMap.get(v1);
+      const v2Edges = this.adjacencyMap.get(v2);
+      if (Array.isArray(v1Edges) && !v1Edges.includes(v2)) {
+        v1Edges.push(v2);
+      }
+      if (Array.isArray(v2Edges) && !v2Edges.includes(v1)) {
+        v2Edges.push(v1);
+      }
+    }
+  };
 
-  Graph.prototype.removeVertex = function (vertex) {};
+  Graph.prototype.removeVertex = function (vertex) {
+    if (this.adjacencyMap.has(vertex)) {
+      this.adjacencyMap.delete(vertex);
+      // eslint-disable-next-line no-restricted-syntax
+      for (const key of this.adjacencyMap.keys()) {
+        this.adjacencyMap.set(
+          key,
+          this.adjacencyMap.get(key).filter((v) => v !== vertex)
+        );
+      }
+    }
+  };
 
   Graph.prototype.DFS = function (vertex) {};
 
