f9bbab67e5c6ce91f74c9b35b6e5cfb80607992b
kunal-singh <hi@thekunalsingh.com>
2024-04-17 15:00:13 +0530

    2D matrix wip

--- Matrix/pre/matrix2D.js
+++ Matrix/pre/matrix2D.js
@@ -1,6 +1,13 @@
 const Matrix2D = (function () {
   // eslint-disable-next-line no-shadow
   function Matrix2D(rows, columns) {
+    this.vectors = [
+      [-1, 0],
+      [1, 0],
+      [0, -1],
+      [0, 1]
+    ];
+    this.visited = new Map();
     if (rows && columns) this.matrix = Array(rows).fill(Array(columns).fill(0));
   }
 
@@ -12,16 +19,35 @@ const Matrix2D = (function () {
     return this.matrix;
   };
 
-  function BFSUtil() {}
+  function isValidCoords(i, j) {
+    if (
+      i < 0 ||
+      j < 0 ||
+      i >= this.matrix.length ||
+      j >= this.matrix[0].length ||
+      this.visited.has(`${i}-${j}`)
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  function BFSUtil(i, j) {}
 
   Matrix2D.prototype.BFS = function (coords) {
     const [i, j] = coords;
+    this.visited = new Map();
+    BFSUtil.call(this, i, j);
   };
 
-  function DFSUtil() {}
+  function DFSUtil(i, j) {
+    console.log(i, j, this.matrix, this.vectors);
+  }
 
   Matrix2D.prototype.DFS = function (coords) {
     const [i, j] = coords;
+    this.visited = new Map();
+    DFSUtil.call(this, i, j);
   };
 
   return Matrix2D;
