151d4874e89bfa1e32c43f742f59766f9ba7f1a8
kunal-singh <hi@thekunalsingh.com>
2026-03-17 22:08:55 +0530

    added reverse

--- Arrays/pre/Reverse.js
+++ Arrays/pre/Reverse.js
@@ -1,4 +1,15 @@
-const Reverse = function () {
-  throw new Error('Not implemented');
+const Reverse = function (array) {
+  if (!Array.isArray(array)) {
+    throw new Error('Input must be an array');
+  }
+  const temp = [];
+  let b = array.length - 1;
+
+  while (b >= 0) {
+    temp[temp.length] = array[b];
+    b -= 1;
+  }
+
+  return temp;
 };
 export default Reverse;
